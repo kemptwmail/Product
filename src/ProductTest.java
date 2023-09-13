da90cc3b8c6bb8c436fe3d4ac0314305141ada4b
kemptwmail <kemptw@mail.uc.edu>
2023-09-12 20:02:16 -0400

    commit

---
+++ src/ProductTest.java
@@ -0,0 +1,8 @@
+public class ProductTest
+{
+    public static void main(String[] args)
+    {
+        Product tyler = new Product("000001","Tyler","Famous great apples",20.0);
+        System.out.println(tyler);
+    }
+}
